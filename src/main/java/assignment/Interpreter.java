ec1716f108fcde4da65a8169f0c73aeda914c73a
Aryan Khatri <aryan.khatri@tigeranalytics.com>
2022-09-14 14:00:08 -0500

    testing commit

--- src/main/java/assignment/Interpreter.java
+++ src/main/java/assignment/Interpreter.java
@@ -12,7 +12,7 @@ import java.io.IOException;
 public class Interpreter implements CritterInterpreter {
 
 	public void executeCritter(Critter c) {
-		// obviously, your code should do something
+		System.out.println("test");
 		return;
 	}
 
