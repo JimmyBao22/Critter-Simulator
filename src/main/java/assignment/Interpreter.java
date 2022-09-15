96f4844a0b07c3dc498aa99a8204d891d572e70e
Jimmy <jimmybao22@gmail.com>
2022-09-15 15:41:28 -0500

    commit switch cases

--- src/main/java/assignment/Interpreter.java
+++ src/main/java/assignment/Interpreter.java
@@ -31,7 +31,73 @@ public class Interpreter implements CritterInterpreter {
 
 			switch (operation) {
 				case "hop":
-					instructions.add(new Hop(arguments.sub))
+					instructions.add(new Hop(arguments));
+					break;
+				case "left":
+					instructions.add(new Left(arguments));
+					break;
+				case "right":
+					instructions.add(new Right(arguments));
+					break;
+				case "infect":
+					instructions.add(new Infect(arguments));
+					break;
+				case "eat":
+					instructions.add(new Eat(arguments));
+					break;
+				case "go":
+					instructions.add(new Go(arguments));
+					break;
+				case "ifrandom":
+					instructions.add(new IfRandom(arguments));
+					break;
+				case "ifhungry":
+					instructions.add(new IfHungry(arguments));
+					break;
+				case "ifstarving":
+					instructions.add(new IfStarving(arguments));
+					break;
+				case "ifempty":
+					instructions.add(new IfEmpty(arguments));
+					break;
+				case "ifally":
+					instructions.add(new IfAlly(arguments));
+					break;
+				case "ifenemy":
+					instructions.add(new IfEnemy(arguments));
+					break;
+				case "ifwall":
+					instructions.add(new IfWall(arguments));
+					break;
+				case "ifangle":
+					instructions.add(new IfAngle(arguments));
+					break;
+				case "write":
+					instructions.add(new Write(arguments));
+					break;
+				case "add":
+					instructions.add(new Add(arguments));
+					break;
+				case "sub":
+					instructions.add(new Sub(arguments));
+					break;
+				case "inc":
+					instructions.add(new Inc(arguments));
+					break;
+				case "dec":
+					instructions.add(new Dec(arguments));
+					break;
+				case "iflt":
+					instructions.add(new IfLt(arguments));
+					break;
+				case "ifeq":
+					instructions.add(new IfEq(arguments));
+					break;
+				case "ifgt":
+					instructions.add(new IfGt(arguments));
+					break;
+				default:
+					// TODO: throw error
 					break;
 			}
 			line = br.readLine();
